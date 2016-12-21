5147cdc4b515c325079be109c3ff648894a60e87
Licoffe <p1lgr11m@gmail.com>
2016-12-21 16:10:25 +0100

    Remove c++ indexer code

--- indexer-ng/Requirement.cpp
+++
@@ -1,6 +0,0 @@
-#include "Requirement.h"
-
-Requirement::Requirement( std::string name, float value ) {
-    this->name  = name;
-    this->value = value; 
-}
--- indexer-ng/Socket.cpp
+++
@@ -1,6 +0,0 @@
-#include "Socket.h"
-
-Socket::Socket( int group, char attr ) {
-    this->group = group;
-    this->attr  = attr;
-}
